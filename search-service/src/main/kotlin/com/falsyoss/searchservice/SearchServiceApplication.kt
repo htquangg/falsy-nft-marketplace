7d3149ec222e0db9457519ff3297242f3cdf96b1
Tuan Nguyen <40360299+uuhnaut69@users.noreply.github.com>
2023-04-08 18:05:16 +0700

    [SEARCH-SERVICE] Init search service (#14)

---
+++ search-service/src/main/kotlin/com/falsyoss/searchservice/SearchServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.falsyoss.searchservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SearchServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<SearchServiceApplication>(*args)
+}
---
+++ search-service/src/test/kotlin/com/falsyoss/searchservice/SearchServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.falsyoss.searchservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SearchServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
