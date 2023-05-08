c5232e584b6af80d6f28f3bb21f0cfb26a32a089
Tuan Nguyen <40360299+uuhnaut69@users.noreply.github.com>
2023-05-08 07:59:10 +0700

    chore: Restructure project directory (#70)

--- src/local.sh
+++ src/local.sh
@@ -3,9 +3,10 @@ docker compose down -v
 
 docker compose up -d
 
-sleep 120
-
 CREATE_CQL="CREATE KEYSPACE IF NOT EXISTS account_keyspace WITH REPLICATION = {'class': 'NetworkTopologyStrategy', 'replication_factor': 1};"
 echo "Executing: $CREATE_CQL"
 
-docker exec account-database bin/bash cqlsh -e "$CREATE_CQL";
+until docker exec account-database bin/bash cqlsh -e "$CREATE_CQL"; do
+    echo "Database unavailable: retry after 5 seconds"
+    sleep 5
+done &
