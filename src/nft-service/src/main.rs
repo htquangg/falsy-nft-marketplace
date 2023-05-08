5ce01b455a15071798f38c7e3d212a72e49f425d
Tuan Nguyen <40360299+uuhnaut69@users.noreply.github.com>
2023-05-08 22:02:07 +0700

    feat: Dockerize nft service (#81)

--- src/nft-service/src/main.rs
+++ src/nft-service/src/main.rs
@@ -1,4 +1,31 @@
+use std::net::SocketAddr;
+
+use axum::{
+    http::StatusCode,
+    Json,
+    response::IntoResponse,
+    Router, routing::{get, post},
+};
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+
+    // build our application with a route
+    let app = Router::new();
+
+    // run our app with hyper, listening globally on port 3000
+    axum::Server::bind(&"0.0.0.0:3333".parse().unwrap())
+        .serve(app.into_make_service())
+        .with_graceful_shutdown(signal_shutdown())
+        .await
+        .unwrap();
 }
+
+async fn signal_shutdown() {
+    tokio::signal::ctrl_c()
+        .await
+        .expect("expect tokio signal ctrl-c");
+}
+
